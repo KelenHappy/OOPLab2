0b738979dd5cf5834be68a05b72414c702aecdbe
112590004 <git@gitea.is1ab.com:112590004/oop2024f_112590004_hw.git>
2024-12-27 19:51:03 +0800

    a

--- runtime/Runtime.cpp
+++ runtime/Runtime.cpp
@@ -35,32 +35,27 @@ void RuntimeFramework::Running() {
     inky->Move(player->GetPosition());
     pinky->Move(player->GetPosition());
     if (player->GetPosition() == blinky->GetPosition() || player->GetPosition() == clyde->GetPosition() || player->GetPosition() == inky->GetPosition()
-<<<<<<< Updated upstream
     || player->GetPosition() == pinky->GetPosition()) {  // player point
         Map::Draw(game_objects);
         std::cout << "You lose." << std::endl;
         RuntimeFramework::End();  
     }
-   /* if(point_manager->IsOverlapping()){
-        RuntimeFramework::End();
-    }*/
-=======
-    || player->GetPosition() == pinky->GetPosition()) {  
-        RuntimeFramework::End();  
-    }
- 
+    /* if(point_manager->IsOverlapping()){
+         RuntimeFramework::End();
+     }*/
+
     if (point_manager->GetPointListSize() == 0) {
         std::cout << "All points collected! You win!" << std::endl;
         End();
     }
->>>>>>> Stashed changes
 }
 
-void RuntimeFramework::Render() { //using Draw()
-    Map::Draw(game_objects);
-}
+    void RuntimeFramework::Render() { //using Draw()
+        Map::Draw(game_objects);
+    }
 
-void RuntimeFramework::End() {
-    state = RuntimeState::END;
-}
-};  // namespace Util
+    void RuntimeFramework::End() {
+        state = RuntimeState::END;
+
+    };
+} // namespace Util
