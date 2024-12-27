67f4d004e476d35f4a71d29061939945417e35e7
112590004 <git@gitea.is1ab.com:112590004/oop2024f_112590004_hw.git>
2024-12-27 19:39:34 +0800

    a

--- runtime/Runtime.cpp
+++ runtime/Runtime.cpp
@@ -8,14 +8,18 @@
 
 namespace Util {
 
+
 void RuntimeFramework::Initial() {
+    std::vector<Object::GamePosition> initial_points = {
+        {2, 8}, {9, 9}, {8, 3}, {5, 7}
+    };
     game_objects.push_back(player);
     game_objects.push_back(blinky);
     game_objects.push_back(clyde);
     game_objects.push_back(inky);
     game_objects.push_back(pinky);
-    auto points = point_manager->GetPointList(); 
-    game_objects.insert(game_objects.end(), points.begin(), points.end());
+    //auto points = point_manager->GetPointList();
+    //game_objects.insert(game_objects.end(), points.begin(), points.end());
     
 
 }
@@ -30,11 +34,12 @@ void RuntimeFramework::Running() {
     pinky->Move(player->GetPosition());
     if (player->GetPosition() == blinky->GetPosition() || player->GetPosition() == clyde->GetPosition() || player->GetPosition() == inky->GetPosition()
     || player->GetPosition() == pinky->GetPosition()) {  // player point
+        Map::Draw(game_objects);
         RuntimeFramework::End();  
     }
-    if(point_manager->IsOverlapping()){
+   /* if(point_manager->IsOverlapping()){
         RuntimeFramework::End();
-    }
+    }*/
 }
 
 void RuntimeFramework::Render() { //using Draw()
