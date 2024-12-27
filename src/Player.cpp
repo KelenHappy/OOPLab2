ca348790b02b128a8a3d09fac7d74e6e8f33056a
Kelen <88041456+KelenHappy@users.noreply.github.com>
2024-12-27 19:04:46 +0800

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -17,22 +17,22 @@ void Player::Move(char direction) {
             case 'w':
             case 'W':
                 //setPosition(position.x -3, position.y );  // 向上移動
-                pos.y += 3;               
+                pos.y += 1;               
                 break;
             case 'a':
             case 'A':
                 //setPosition(position.x , position.y -3);  // 向左移動
-                pos.x -= 3;
+                pos.x -= 1;
                 break;
             case 's':
             case 'S':
                 //setPosition(position.x + 3, position.y );  // 向下移動
-                pos.y -= 3;
+                pos.y -= 1;
                 break;
             case 'd':
             case 'D':
                 //setPosition(position.x, position.y +3);  // 向右移動
-                pos.x += 3;
+                pos.x += 1;
                 break;
             default:
                 break;  // 不處理其他字符
